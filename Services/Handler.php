4b9f96139a8822d06e5fdf801aa78d1f7f3d0859
Olivier Mouren <mouren.olivier@gmail.com>
2020-08-05 11:29:43 +0200

    Update dependencies

--- Services/Handler.php
+++ Services/Handler.php
@@ -13,9 +13,6 @@ class Handler
 
     private $htmlContentDumper;
 
-    private $events = array();
-
-
     public function __construct(Client $client)
     {
         $this->client = $client;
@@ -24,17 +21,6 @@ class Handler
 
     public function handleEvent(ExternalVarDumpEvent $event)
     {
-        //if ('cli' === PHP_SAPI) {
-            $this->client->sendDump($this->htmlContentDumper->getDump($event->getVar()), $event->getSource(), $event->getDatetime());
-        //} else {
-        //    $this->events[] = $event;
-        //}
-    }
-
-    public function onKernelTerminate()
-    {
-        foreach ($this->events as $event) {
-            $this->client->sendDump($this->htmlContentDumper->getDump($event->getVar()), $event->getSource(), $event->getDatetime());
-        }
+        $this->client->sendDump($this->htmlContentDumper->getDump($event->getVar()), $event->getSource(), $event->getDatetime());
     }
 }
